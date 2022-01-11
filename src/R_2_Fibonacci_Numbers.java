ab1e1be01b18307b3d92c69463d0ea9ea43af496
Rumen Vasilev <vasilev.rumen.97@gmail.com>
2022-01-11 20:56:07 +0200

    Fixed Fibonacci Numbers to be even more efficient.

--- src/R_2_Fibonacci_Numbers.java
+++ src/R_2_Fibonacci_Numbers.java
@@ -7,20 +7,17 @@ public class R_2_Fibonacci_Numbers {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int f1;
-        int f2 = 1;
+        int f2 = 0;
         int f3 = 1;
 
-        if (N >= 1) {
-            System.out.print("0, 1");
+        if (N >= 0) {
+            System.out.print(0);
             while (f3 <= N) {
                 System.out.printf(", %d", f3);
                 f1 = f2;
                 f2 = f3;
                 f3 = f1 + f2;
             }
-        } else if (N==0) {
-            System.out.println(0);
         }
-
     }
 }
